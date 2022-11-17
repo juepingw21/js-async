5b1aa746977e42b86dbaa18be1744b25d57dd557
Jueping Wang <jueping.wang21@gmail.com>
2022-11-17 01:54:00 -0800

    update

--- 16-promise-queue/queue.js
+++ 16-promise-queue/queue.js
@@ -1,3 +1,28 @@
+// let promise = Promise.resolve("1");
+// promise
+//     .then((value) => {
+//         console.log(value);
+//         return new Promise((resolve)=>{
+//             resolve("2");
+//         })
+//     })
+//     .then((value) => {
+//         console.log(value);
+//     });
+
+let promise = Promise.resolve("1");
+
+promise = promise.then((value) => {
+    console.log(value);
+    return new Promise((resolve) => {
+        resolve("2");
+    });
+});
+
+promise = promise.then((value) => {
+    console.log(value);
+});
+
 // function queue(nums) {
 //     let promise = Promise.resolve();
 //     nums.map((value) => {
@@ -14,25 +39,25 @@
 
 // queue([1,2,3,4,5]);
 
-function p1(){
-    return new Promise(resolve=>{
-        setTimeout(()=>{
+function p1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
             console.log("p1");
             resolve();
-        }, 1000);
-    })
+        }, 3000);
+    });
 }
 
-function p2(){
-    return new Promise(resolve=>{
-        setTimeout(()=>{
+function p2() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
             console.log("p2");
             resolve();
         }, 1000);
-    })
+    });
 }
 
-function queue(nums) {
+function queueMap(nums) {
     let promise = Promise.resolve();
     nums.map((value) => {
         promise = promise.then(() => {
@@ -41,4 +66,22 @@ function queue(nums) {
     });
 }
 
-queue([p1, p2]);
+function queueReduce(nums) {
+    nums.reduce((promise, value) => {
+        return promise.then(() => {
+            return value();
+        });
+    }, Promise.resolve());
+}
+
+function queue(tasks) {
+    let curr = Promise.resolve();
+    for (let i = 0; i < tasks.length; i++) {
+        curr = curr.then(() => {
+            return tasks[i]();
+        });
+    }
+}
+// queueMap([p1, p2]);
+// queueReduce([p1, p2]);
+// queue([p1, p2]);
